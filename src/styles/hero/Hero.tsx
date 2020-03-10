27070fa36f7c0ab6b379a0346abc5655bee60c63
Masiu <ciszekmarcell@gmail.com>
2020-03-10 15:29:09 +0100

    add

--- src/styles/hero/Hero.tsx
+++ src/styles/hero/Hero.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import BackgroundImage from 'gatsby-background-image'
+import styled from 'styled-components'
 
 interface P {
   img: string
@@ -9,7 +10,21 @@ interface P {
 }
 
 const Hero: React.FC<P> = ({ img, className, children, home }) => {
-  return <div> {children} </div>
+  return (
+    <BackgroundImage fluid={img} home={home} className={className}>
+      {' '}
+      {children}{' '}
+    </BackgroundImage>
+  )
 }
 
-export default Hero
+export default styled(Hero)`
+  min-height: ${props => (props.home ? `calc(100vh - 62px)` : `50vh`)};
+  background: ${props => (props.home ? `linear-gradient(rgba(2, 8, 2, 0.2), rgba(0, 0, 0, 0.7))` : `none`)};
+  background-position: center;
+  background-size: cover;
+  opacity: 1 !important;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
